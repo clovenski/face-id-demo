f7b987b56bff47fd17a94dda695e33d6976e1070
Joel Tengco <joeltengco@yahoo.com>
2018-11-04 23:42:14 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# face-id-demo
+
+A program that provides a demo for facial recognition using dlib and OpenCV.
+
+## Overview
+
+Using OpenCV to capture data from a webcam, you can capture a photo of yourself to be used for your facial identification. Then, you are allowed to take more pictures of yourself to be included in the demo. Once you are satisfied with your photoshoot, the program will sift through an images directory to detect faces and predict whether they are you or not.
+
+## Environment
+
+My environment used to develop this project:
+
+- Ubuntu 18.04.1
+- Python 2.7.15rc1
+- dlib 19.16.99
+- OpenCV 3.2.0
+- NumPy 1.15.2
+
+## Usage
+
+First, make sure you have an `images` directory where you can store images to be used in the demo. Also, download the appropriate models from dlib:
+-  http://dlib.net/files/shape_predictor_5_face_landmarks.dat.bz2
+- http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2
+
+Now, run the program with this command:
+
+`./main.py [path to shape predictor (first link)] [path to face recognizer (second link)] [path to images dir]`
+
+Throughout the entirety of the program, pressing the 'q' key will exit the program.
+
+At this point, data from your usb webcam should pop up. If not, then try changing the `1` in `cap = cv.VideoCapture(1)` to `0`.
+
+When you are ready to take a picture of yourself, press 'c' on your keyboard to use that picture to compute your facial identification. The webcam feed will most likely freeze; that's normal, it is now computing your id.
+
+Then, once it is done you are allowed to take additional photos of yourself to be included in the demo. Once you are satisfied with your photoshoot, press the 'p' key to start the demo.
+
+The program should then iterate through all of your .jpg images in the `images` directory, detecting faces and making a prediction to whether that face is yours or not. Green bounding boxes are a match and red bounding boxes are not a match.
+
+Once that is done, you will be returned to the start of the program where you can then use another face to be used for this demo. Note that at this point your photoshoot photos are deleted.
+
+Quitting the program with the 'q' key will also delete any of your photoshoot photos before exiting the program.
---
+++ main.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+
+import sys
+import os
+import dlib
+import glob
+import cv2 as cv
+import numpy as np
+from math import sqrt
+
+if len(sys.argv) != 4:
+    print(
+        "Call this program like this:\n"
+        "   ./main.py shape_predictor_5_face_landmarks.dat dlib_face_recognition_resnet_model_v1.dat images\n"
+        "You can download a trained facial shape predictor and recognition model from:\n"
+        "    http://dlib.net/files/shape_predictor_5_face_landmarks.dat.bz2\n"
+        "    http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2")
+    exit()
+
+predictor_path = sys.argv[1]
+face_rec_model_path = sys.argv[2]
+faces_folder_path = sys.argv[3]
+
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor(predictor_path)
+facerec = dlib.face_recognition_model_v1(face_rec_model_path)
+
+def euclDistance(face1, face2):
+    result = face1 - face2
+    result = result ** 2
+    result = np.sum(result)
+    return sqrt(result)
+
+def drawBox(shape, match):
+    global img
+
+    color = (0,255,0) if match else (0,0,255)
+    box_pt1 = (shape.rect.tl_corner().x, shape.rect.tl_corner().y)
+    box_pt2 = (shape.rect.br_corner().x, shape.rect.br_corner().y)
+    cv.rectangle(img, box_pt1, box_pt2, color)
+
+def processImages():
+    global face_id, img
+
+    for f in glob.glob(os.path.join(faces_folder_path, '*.jpg')):
+        img = dlib.load_rgb_image(f)
+
+        dets = detector(img, 1)
+
+        shapes = []
+        matches = []
+        for d in dets:
+            shape = sp(img, d)
+            shapes.append(shape)
+
+            face_id2 = np.array(facerec.compute_face_descriptor(img, shape))
+            if euclDistance(np.array(face_id), face_id2) < 0.47:
+                matches.append(True)
+            else:
+                matches.append(False)
+
+        img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+        for i, box in enumerate(shapes):
+            drawBox(box, matches[i])
+        while True:
+            cv.imshow(f, img)
+            key = cv.waitKey(1) & 0xFF
+            if key == ord('n'):
+                break
+            elif key == ord('q'):
+                cap.release()
+                cv.destroyAllWindows()
+                cleanUpImgDir()
+                exit()
+        cv.destroyAllWindows()
+
+def computeFaceId(img):
+    global face_id
+
+    dets = detector(img, 0)
+    if len(dets) != 1:
+        face_id = None
+    else:
+        shape = sp(img, dets[0])
+        face_id = facerec.compute_face_descriptor(img, shape, 50)
+
+def cleanUpImgDir():
+    for f in glob.glob(os.path.join(faces_folder_path, 'photo_of_you_*')):
+        os.remove(f)
+
+cap = cv.VideoCapture(1)
+cap.set(cv.CAP_PROP_FPS, 60.0)
+
+calibrated = False
+img_num = 1
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret == True:
+        if not calibrated:
+            cv.imshow('Let me identify you!', frame)
+            key = cv.waitKey(1) & 0xFF
+            if key == ord('c'):
+                computeFaceId(frame)
+                calibrated = True if face_id != None else False
+                if calibrated:
+                    cv.destroyWindow('Let me identify you!')
+            elif key == ord('q'):
+                break
+        else:
+            cv.imshow('Take photo number {}!'.format(img_num), frame)
+            key = cv.waitKey(1) & 0xFF
+            if key == ord('c'):
+                cv.imwrite('images/photo_of_you_{}.jpg'.format(img_num), frame)
+                cv.destroyWindow('Take photo number {}!'.format(img_num))
+                img_num += 1
+            elif key == ord('p'):
+                cv.destroyWindow('Take photo number {}!'.format(img_num))
+                processImages()
+                cleanUpImgDir()
+                face_id = None
+                calibrated = False
+                img_num = 1
+            elif key == ord('q'):
+                break
+    else:
+        break
+
+cleanUpImgDir()
+cap.release()
+cv.destroyAllWindows()
